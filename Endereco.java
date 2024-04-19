668ff9849f5e800d43f269f569f71fe3d97cb30e
GiovannaLyssa99 <giovannalyssa@discente.ufg.br>
2024-04-19 14:38:13 -0300

    Criação Endereco

---
+++ Endereco.java
@@ -0,0 +1,8 @@
+public class Endereco {
+    private Integer numero;
+    private String complemento;
+    private Integer CEP;
+    private String nome;
+    private TipoEndereco tipo;
+    private Logradouro logradouro;
+}
